6503ef59bbce7d4986f3fe06202336fa07ea1291
JosueR <jojorissos@gmail.com>
2023-12-05 23:48:55 -0300

    Primer commit

---
+++ java.js
@@ -0,0 +1,22 @@
+// function sacarPromedio () {
+
+//     for(let i = 1; i < 4; i++) {
+    
+//     let nota1 = Number(prompt("alumno nro " + i + " ingrese su primer nota: "));
+//     let nota2 = Number(prompt("alumno nro " + i + " ingrese su segunda nota: "));
+//     let nota3 = Number(prompt("alumno nro " + i + " ingrese su tercer nota: "));
+
+//     let suma = nota1 + nota2 + nota3;
+
+//     let promedio = suma / 3;
+
+//     alert("su promedio de notas es: " + promedio);
+
+// if (promedio >= 6) {
+//     alert("Usted esta aprobado. Felicidades! :)")
+// } else {
+//     alert("Usted esta desaprobado :(")
+
+// } } }
+
+// sacarPromedio();
